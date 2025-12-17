6bafd204c856d76f121c58a4b3ba95ccc822f3ef
Tiger1-creator <olivierniyongabo200@gmail.com>
2025-12-17 17:25:43 +0200

    updated account/app file

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        # This connects your new signals.py file
+        import accounts.signals
